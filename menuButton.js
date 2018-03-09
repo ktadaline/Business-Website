7f4bc149567c8079d782272bb0d1138be5e88516
Katie Adaline Sroub <ksroub@gmail.com>
2018-03-09 00:20:56 -0500

    Drop Down Responsive Nav

--- menuButton.js
+++ menuButton.js
@@ -1,9 +1,16 @@
     function myFunction(){
-        var x = document.getElementsByClassName("right");
-        if(x.className === "right"){
-            x.className += "responsive";
-        }
-        else{
-            x.className = "right";
-        }
+        console.log("test");
+        var x = document.getElementById("menuID");
+        var y = document.getElementById("hideContainerID");
+//update this to be an element ^ not a class name.. see w3 example
+if (x.className === "menu-icon"){
+    x.className = "responsive";
+    y.className = "showContainer";
+
+}else{
+    x.className = "menu-icon";
+    y.className = "hideContainer";
+}
     }
+
+ 
