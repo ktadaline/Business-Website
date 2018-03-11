d3b823957d3ce7debb2f07a3d3f21a1e3b6c2829
Katie Adaline Sroub <ksroub@gmail.com>
2018-03-11 17:32:40 -0400

    project page layout

--- menuButton.js
+++ menuButton.js
@@ -2,6 +2,7 @@
         console.log("test");
         var x = document.getElementById("menuID");
         var y = document.getElementById("hideContainerID");
+      
 //update this to be an element ^ not a class name.. see w3 example
 if (x.className === "menu-icon"){
     x.className = "responsive";
@@ -10,6 +11,7 @@ if (x.className === "menu-icon"){
 }else{
     x.className = "menu-icon";
     y.className = "hideContainer";
+ 
 }
     }
 
