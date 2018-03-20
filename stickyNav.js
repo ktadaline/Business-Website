dc093895b5a4d1e5f86fa096a68a00ced5ab0e68
Katie Adaline Sroub <ksroub@gmail.com>
2018-03-20 01:19:41 -0400

    fixing sticky

--- stickyNav.js
+++ stickyNav.js
@@ -1,10 +1,12 @@
-window.onscroll = function() {myStickyFunc()};
+window.onscroll = function() {stickFunction()};
+
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-function myStickyFunc(){
-    if(window.pageYOffset >= sticky){
-        navbar.classList.add("sticky");
-    } else{
-        navbar.classList.remove("sticky");
-    }
+
+function stickFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
