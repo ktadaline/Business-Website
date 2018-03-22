be522a86cbd974552585559dda8baac0515c4e21
Katie Adaline Sroub <ksroub@gmail.com>
2018-03-21 21:21:13 -0400

    fixing responsiveness and sticky nav

--- stickyNav.js
+++ stickyNav.js
@@ -4,7 +4,7 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function stickFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky-108) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
